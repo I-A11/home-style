a7510d67fda6953483980c9f76a4dda694225e6c
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-11-16 19:03:55 +1100

    Setup components and pages placeholders.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
-import Logo from "./assets/logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import {
+  About,
+  Home,
+  SingleProduct,
+  Products,
+  PrivateRoute,
+  Cart,
+  Error,
+  Checkout,
+} from "./pages";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/products' element={<Products />} />
+        <Route exact path='/products/:id' element={<SingleProduct />} />
+        <Route exact path='/privateRoute' element={<PrivateRoute />} />
+        <Route exact path='/cart' element={<Cart />} />
+        <Route exact path='/*' element={<Error />} />
+        <Route exact path='/checkout' element={<Checkout />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "./AboutPage";
+import Products from "./ProductsPage";
+import Home from "./HomePage";
+import SingleProduct from "./SingleProductPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+
+export {
+  About,
+  Home,
+  SingleProduct,
+  Products,
+  PrivateRoute,
+  Cart,
+  Error,
+  Checkout,
+};
