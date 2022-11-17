df4ed634acbe6b3e9b17fd5c02d347aa0000315a
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-11-17 11:36:38 +1100

    Create navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,47 @@ import React from "react";
 import Logo from "../assets/logo.svg";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { Links } from "../utils/constants";
+import { links, Links } from "../utils/constants";
 import CartButtons from "./CartButtons";
 import { useProductsContext } from "../context/products_context";
 import { useUserContext } from "../context/user_context";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='h-20 flex items-center justify-center mt-3'>
+      <div className='w-[90vw] m-0 mx-auto maxWidth md:lg:grid grid-cols-[1fr_500px_1fr] md:lg:items-center'>
+        <div className='flex items-center justify-between'>
+          <Link to='/'>
+            <img className='w-52 -ml-4 ' src={Logo} alt='logo' />
+          </Link>
+          <button
+            className='bg-transparent border-transparent text-yellow-700 text-3xl cursor-pointer md:lg:hidden'
+            type='button'
+          >
+            <FaBars />
+          </button>
+        </div>
+        <ul className='hidden md:lg:flex justify-center items-center'>
+          {links.map((link) => {
+            const { id, text, url } = link;
+            return (
+              <li className='mx-2' key={id}>
+                <Link
+                  className='text-gray-700 text-base font-semibold capitalize p-2 hover:border-b-2 hover:border-yellow-700'
+                  to={url}
+                >
+                  {text}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <div className='hidden md:lg:grid'>
+          <CartButtons />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/utils/constants.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { VscCompass } from "react-icons/vsc";
+import { SlDiamond } from "react-icons/sl";
+import { MdHistoryEdu } from "react-icons/md";
+export const links = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about",
+  },
+  {
+    id: 3,
+    text: "products",
+    url: "/products",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: <VscCompass />,
+    title: "mission",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 2,
+    icon: <SlDiamond />,
+    title: "vision",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 3,
+    icon: <MdHistoryEdu />,
+    title: "history",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+];
+
+export const products_url = "https://course-api.com/react-store-products";
+
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`;
