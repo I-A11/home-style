c3ebb6aa6743a6fffa3ca6394281a28e59692fc7
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-11-23 12:10:41 +1100

    Create error page.

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className='bg-blue-200 flex justify-center items-center text-center min-h-[calc(100vh-10.5rem)] py-10'>
+      <div>
+        <h1 className='text-[10rem] font-semibold'>404</h1>
+        <h3 className='mb-8 text-4xl'>
+          Sorry, the page you tried cannot be found
+        </h3>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
