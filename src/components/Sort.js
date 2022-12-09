476bbb690254c8eb503ddd1049dca6429d181db5
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-09 11:23:15 +1100

    Complete sort functionality in products' page.

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -3,7 +3,14 @@ import { useFilterContext } from "../context/filter_context";
 import { BsFillGridFill, BsList } from "react-icons/bs";
 
 const Sort = () => {
-  const { filtered_products: products, grid_view } = useFilterContext();
+  const {
+    filtered_products: products,
+    grid_view,
+    setGridView,
+    setListView,
+    sort,
+    updateSort,
+  } = useFilterContext();
   return (
     <div className='mt-4 lg:mt-16 mb-8 grid grid-cols-[1fr] sm:gap-y-3 sm:grid-cols-[auto_auto_1fr_auto] sm:items-center sm:gap-x-8 '>
       <div className='grid w-16 grid-cols-2 mb-4 sm:mb-0 '>
@@ -12,6 +19,7 @@ const Sort = () => {
           className={`bg-transparent border border-black w-6 rounded h-6 flex justify-center items-center cursor-pointer ${
             grid_view ? "bg-black text-white" : null
           }`}
+          onClick={setGridView}
         >
           <BsFillGridFill className='text-base' />
         </button>
@@ -20,6 +28,7 @@ const Sort = () => {
           className={`bg-transparent border border-black w-6 rounded h-6 flex justify-center items-center cursor-pointer ${
             !grid_view ? "bg-black text-white" : null
           }`}
+          onClick={setListView}
         >
           <BsList className='text-base' />
         </button>
@@ -27,7 +36,7 @@ const Sort = () => {
       <p className='font-semibold mb-2 lg:mb-0'>
         {products.length} products found
       </p>
-      <hr className='mb-8 lg:mb-0' />
+      <hr className='mt-8 mb-8 lg:mt-0 lg:mb-0' />
       <form>
         <label className='capitalize font-semibold' htmlFor='sort'>
           sort by
@@ -36,11 +45,13 @@ const Sort = () => {
           name='sort'
           id='sort'
           className='border-transparent text-base capitalize px-2 py-1'
+          value={sort}
+          onChange={updateSort}
         >
           <option value='price-lowest'>price (lowest)</option>
           <option value='price-highest'>price (highest)</option>
-          <option value='price-name-a'>name (a - z)</option>
-          <option value='price-name-z'>name (z - a)</option>
+          <option value='name-a'>name (a - z)</option>
+          <option value='name-z'>name (z - a)</option>
         </select>
       </form>
     </div>
