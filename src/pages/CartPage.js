6a917b6261b1a307ef05b908a0ab7d7f27610fb9
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-16 12:53:26 +1100

    Create local storage for cart item.

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
+import { CartContent, PageHero } from "../components";
 
 const CartPage = () => {
-  return <div>CartPage</div>;
+  const { cart } = useCartContext();
+
+  if (cart.length < 1) {
+    return (
+      <div className='page-100'>
+        <div className='text-center'>
+          <h2 className='text-3xl mb-4 font-semibold mt-10'>
+            Your cart is empty
+          </h2>
+          <Link to='/products' className='btn'>
+            fill it
+          </Link>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <main>
+      <PageHero title='cart' />
+      <div className='page'>
+        <CartContent />
+      </div>
+    </main>
+  );
 };
 
 export default CartPage;
