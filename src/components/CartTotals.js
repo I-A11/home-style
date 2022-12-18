90ea159e724d9091f278dd0fd3477362a542f112
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-18 13:21:05 +1100

    Setup cart total component for cart page.

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
+import { formatPrice } from "../utils/helpers";
+import { Link } from "react-router-dom";
 
 const CartTotals = () => {
-  return <div>CartTotals</div>;
+  const { total_amount, shipping_fee } = useCartContext();
+  return (
+    <div className='mt-12 flex justify-center flex-col w-96'>
+      <article className='border border-gray-800 py-6 px-12 rounded-md '>
+        <h5 className='grid grid-cols-[200px_1fr] font-semibold capitalize'>
+          subtotal: <span>{formatPrice(total_amount)}</span>
+        </h5>
+        <p className='grid grid-cols-[200px_1fr] text-sm font-semibold text-gray-700 capitalize mb-3'>
+          shipping fee : <span>{formatPrice(shipping_fee)}</span>
+        </p>
+        <hr />
+        <h4 className='grid grid-cols-[200px_1fr] font-semibold capitalize mt-4'>
+          order total: <span>{formatPrice(total_amount + shipping_fee)}</span>
+        </h4>
+      </article>
+      <Link to='/checkout' className='btn mt-6 mx-auto w-64 text-center'>
+        proceed to checkout
+      </Link>
+    </div>
+  );
 };
 
 export default CartTotals;
