f765d8a82d5f919d5ecb6eb12a166b07c97b2a2d
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-12-18 13:01:25 +1100

    Setup cart columns component.

--- src/components/CarContent.js
+++ src/components/CarContent.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
+import CartColumns from "./CartColumns";
+import CartItem from "./CartItem";
+import CartTotals from "./CartTotals";
 
 const CarContent = () => {
-  return <div>CarContent</div>;
+  const { cart, clearCart } = useCartContext();
+
+  return (
+    <div className='section section-center'>
+      <CartColumns />
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <hr />
+      <div className='flex justify-between mt-8'>
+        <Link
+          to='/products'
+          className='bg-blue-700 text-white capitalize px-2 py-1 rounded-md '
+        >
+          continue shopping
+        </Link>
+
+        <button
+          type='button'
+          className='bg-black text-white capitalize px-2 py-1 rounded-md '
+          onClick={clearCart}
+        >
+          clear shopping cart
+        </button>
+      </div>
+      <CartTotals />
+    </div>
+  );
 };
 
 export default CarContent;
--- src/components/CartColumns.js
+++ src/components/CartColumns.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const CartColumns = () => {
-  return <div>CartColumns</div>;
+  return (
+    <div className='hidden md:block'>
+      <div className='grid grid-cols-[316px_1fr_1fr_1fr_auto] items-center gap-4'>
+        <h5 className='text-gray-700 capitalize font-semibold'>item</h5>
+        <h5 className='text-gray-700 capitalize font-semibold'>price</h5>
+        <h5 className='text-gray-700 capitalize font-semibold'>quantity</h5>
+        <h5 className='text-gray-700 capitalize font-semibold'>subtotal</h5>
+        <h5 className='text-gray-700 capitalize font-semibold'>
+          <span></span>{" "}
+        </h5>
+      </div>
+    </div>
+  );
 };
 
 export default CartColumns;
